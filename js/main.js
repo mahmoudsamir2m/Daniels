2c7fd56e1a0b543ee6d7dac65f4e05a04918c0f5
Mahmoud Samir <youremail@example.com>
2025-07-03 13:02:14 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
-window.addEventListener("scroll", (event) => {
-  console.log(window.scrollY);
-  if (window.scrollY > 500) {
+const aboutSection = document.querySelector("section#about");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > aboutSection.offsetTop-100) {
     document.querySelector("nav").classList.add("nav-scroll");
+    document.querySelectorAll("#nav-list a[class]").forEach((el) => {
+      el.classList.add("bg-after");
+    });
     document.querySelector("nav").classList.remove("navbar-dark");
   } else {
     document.querySelector("nav").classList.remove("nav-scroll");
+    document.querySelectorAll("#nav-list a[class]").forEach((el) => {
+      el.classList.remove("bg-after");
+    });
     document.querySelector("nav").classList.add("navbar-dark");
   }
 });
